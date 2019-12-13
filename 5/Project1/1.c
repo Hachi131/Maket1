fb521ffcec817c7a3949858f906228400b2eaa8b
Hachi131 <58557657+Hachi131@users.noreply.github.com>
2019-12-13 23:16:27 +0800

    5

---
+++ 5/Project1/1.c
@@ -0,0 +1,15 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	int a = 111, b = 1;
+	char c = 't';
+	scanf("%d%d", &a, &b);
+	scanf("%c",& c); 
+	printf("%d\n%d\n%c\n", a, b, c);
+	return 0;
+
+
+	
+}
