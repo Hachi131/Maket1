b28c276d49d16fe4c963d85f9a2954c38e1ddb7e
Hachi131 <58557657+Hachi131@users.noreply.github.com>
2019-12-23 21:07:39 +0800

    15
    直接排序法

---
+++ 15/Project2/2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int zjpx(int s[], int n) {  //自定义函数zjpx
+	int i, j;
+	for (i = 2; i <= n; i++) {  //数组下标从2开始，s[0]做监视哨，s[1]一个数据无可比性
+		s[0] = s[i];  //给监视哨赋值
+		j = i - 1;    //确定要比较元素的最右边位
+		while(s[0]<s[j])
+		{
+			s[j + 1] = s[j];  //数据右移
+				j--;  //产移向左边一个未比较的数
+		}
+		s[j + 1] = s[0];  //在确定的位置插入s[i]
+	}
+	return 0;
+}
+int main() {
+	int a[11], i;  //定义数组及变量为基本整型
+	printf("输入10个数值：\n");
+	for (i=1;i<=10;i++)
+		scanf_s("%d", &a[i]);
+	printf("原始顺序：\n");
+	for (i = 1; i < 11; i++)
+		printf("%5d", a[i]);  //将未排序前的顺序输出
+	zjpx(a, 10);              //调用自定义函数zjpx
+	printf("\n插入数值排序后的顺序\n"); 
+	for (i = 1; i < 11; i++)
+		printf("%3d", a[i]);  //将排序后的数组输出
+	printf("\n");
+	system("pause");
+	return 0;
+
+}
